98c3710e8bde6f5a372c508896c11e876e802c74
deadshumz <shumzz@bk.ru>
2024-11-10 20:31:56 +0300

    ci: fix deploy sh

--- deploy.sh
+++ deploy.sh
@@ -6,21 +6,6 @@ REMOTE_PATH=$REMOTE_PATH
 DOCKER_COMPOSE_FILE="docker-compose.dev.yaml"
 
 echo "Copying files to remote server..."
-<<<<<<< HEAD
-scp -r * "$REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH"
-
-echo "Restarting containers..."
-ssh "$REMOTE_USER@$REMOTE_HOST" << EOF
-    cd $REMOTE_PATH
-    
-    echo "Stoping containers.."
-    docker compose -f $DOCKER_COMPOSE_FILE down
-  
-    echo "Removing useless images.."
-    docker image prune -f
-    
-    echo "Rebuilding and starting new containers.."
-=======
 scp -o StrictHostKeyChecking=no -r * "$REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH"
 
 echo "Restarting containers..."
@@ -34,7 +19,6 @@ ssh -o StrictHostKeyChecking=no "$REMOTE_USER@$REMOTE_HOST" << EOF
     docker image prune -f
     
     echo "Rebuilding and starting new containers..."
->>>>>>> 92d7513 (ci: fix deploy sh)
     docker compose -f $DOCKER_COMPOSE_FILE up -d --build
 EOF
 
