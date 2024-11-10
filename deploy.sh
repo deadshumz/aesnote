14716eac80093b7e91076c3fb7a8fabe8aae00fc
deadshumz <shumzz@bk.ru>
2024-11-10 20:35:29 +0300

    ci: remove useless output deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,20 +6,20 @@ REMOTE_PATH=$REMOTE_PATH
 DOCKER_COMPOSE_FILE="docker-compose.dev.yaml"
 
 echo "Copying files to remote server..."
-scp -o StrictHostKeyChecking=no -r * "$REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH"
+scp -o StrictHostKeyChecking=no -r -q * "$REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH"
 
 echo "Restarting containers..."
 ssh -o StrictHostKeyChecking=no "$REMOTE_USER@$REMOTE_HOST" << EOF
     cd $REMOTE_PATH
     
     echo "Stopping containers..."
-    docker compose -f $DOCKER_COMPOSE_FILE down
-  
+    docker compose -f $DOCKER_COMPOSE_FILE down > /dev/null 2>&1
+    
     echo "Removing useless images..."
-    docker image prune -f
+    docker image prune -f > /dev/null 2>&1
     
     echo "Rebuilding and starting new containers..."
-    docker compose -f $DOCKER_COMPOSE_FILE up -d --build
+    docker compose -f $DOCKER_COMPOSE_FILE up -d --build > /dev/null 2>&1
 EOF
 
 echo "Deploying complete!"
